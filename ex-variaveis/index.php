ac2d6617e0b33e4f9c3c985a3d8c2ded8abc1e5f
joaolollob <joaopedrodelollo01@gmail.com>
2024-12-17 14:28:15 -0300

    Create index.php

---
+++ ex-variaveis/index.php
@@ -0,0 +1,20 @@
+<?php 
+    $nome = "Gustavo";
+    $sobrenome = "Guanabara";
+    const PAIS = "Brasil";
+    //
+    $valor = 200;
+    //
+    $nomeCompletoCliente = "Camel Case";
+    $nome_completo_cliente = "Snake Case";
+    $nomecompletocliente = "";
+    //
+    //
+    echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+    //
+    echo "O valor é igula a $valor no programa";
+    //
+    //
+    //
+     
+?>
