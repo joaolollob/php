18367c30144829c862b51e56fac5d79b80d1a1e4
joaolollob <joaopedrodelollo01@gmail.com>
2024-12-23 14:17:26 -0300

    introdução desafios

---
+++ desafios/d001/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 1</title>
+</head>
+<body>
+    <?php 
+        $num = 25;  
+
+        echo "O antecessor de $num é ". ($num -1) . "\n";
+        echo "O antecessor de $num é ". ($num +1) . "\n";
+
+    ?>
+</body>
+</html>
